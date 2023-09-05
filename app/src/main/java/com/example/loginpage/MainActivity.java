017c59d34aee72705971e21315881912b48e6bbd
Cristian <cdjyanez13@gmail.com>
2023-09-04 23:40:50 -0300

    Se agregaron nuevas Funciones

--- app/src/main/java/com/example/loginpage/MainActivity.java
+++ app/src/main/java/com/example/loginpage/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.loginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usuario, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Asignacion de valores
+        usuario = (EditText)findViewById(R.id.inputName);
+        password = (EditText)findViewById(R.id.inputPassword);
+
     }
+    public void enviarDatos (View view){
+        String valor = usuario.getText().toString();
+        Bundle variables = new Bundle();
+        variables.putString("valorKey",valor);
+        Intent actividad2 = new Intent (this, secondActivity.class);
+        actividad2.putExtras(variables);
+        startActivity(actividad2);
+    }
+
 }
--- app/src/main/java/com/example/loginpage/secondActivity.java
+++ app/src/main/java/com/example/loginpage/secondActivity.java
@@ -3,6 +3,7 @@ package com.example.loginpage;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class secondActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class secondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        // Get the name from the intent
+        String nombre = getIntent().getStringExtra("valorKey");
+
+        // Display the name
+        TextView textView = findViewById(R.id.txtname);
+        textView.setText(nombre);
     }
 }
